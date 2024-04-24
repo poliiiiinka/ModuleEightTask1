2eda91d72faec116c0bcd23cb116e0733cf2fa8d
Полина Артамонова <poliiiiinka71@gmail.com>
2024-04-24 17:20:31 +0300

    Добавьте файлы проекта.

---
+++ ModuleEightTask1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace ModuleEightTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetFolder();
+        }
+
+        static void GetFolder()
+        {
+            Console.Write("Введите адрес папки: ");
+            var FolderName = Console.ReadLine();
+            var dir = new DirectoryInfo(FolderName);
+
+            try
+            {
+                foreach (FileInfo file in dir.GetFiles())
+                {
+                    /// проверяем время последнего обращения к файлу
+                    if (DateTime.Now - file.LastAccessTime >= TimeSpan.FromMinutes(30))
+                    {
+                        file.Delete();
+                        Console.WriteLine($"Файл {file.Name} удалён");
+                    }
+                    else
+                        Console.WriteLine($"Не удалось удалить файл {file.Name}");
+                }
+
+                foreach (DirectoryInfo direct in dir.GetDirectories())
+                {
+                    /// проверяем время последнего обращения к папке
+                    if (DateTime.Now - direct.LastAccessTime >= TimeSpan.FromMinutes(30))
+                    {
+                        direct.Delete(true); /// удаление папки со всем содержимым
+                        Console.WriteLine($"Папка {direct.Name} удалена");
+                    }
+                    else
+                        Console.WriteLine($"Не удалось удалить папку {direct.Name}");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Не удалось удалить папку: {ex}");
+            }
+        }
+    }
+}
